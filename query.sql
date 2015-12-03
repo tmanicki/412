88a03e9fcc260abdd4ddba09d617336188d8612b
tmanicki <tmanicki@asu.edu>
2015-12-03 01:12:59 -0700

    popular items

--- query.sql
+++ query.sql
@@ -7,11 +7,27 @@
 
 /*
 ** ----------------------------------------------------------------------------
-** 	Unsupervised Employees 
-** 	Retrieve information for each employee who does not have a supervisor.
+** 	Popular Itmes 
+**
+** 	Retrieve information on items that were considered popular. The popular
+**	status for this restuarnt is any item's average rating is >=4
 ** --------------------------------------------------------------------------*/
 
 SELECT M.iName, M.iDescription, M.productID, M.price, M.AvgRating
 FROM iMenu M
 WHERE M.AvgRating >=4
 ORDER BY AvgRating desc;
+
+/*
+** ----------------------------------------------------------------------------
+** 	Popular Itmes by amount ordered
+**
+** 	Retrieve information on items that were considered popular by times ordered. 
+**  The popular status for this restuarnt is any item ordered >=50
+** --------------------------------------------------------------------------*/
+
+
+SELECT M.iName, M.iDescription, M.productID, M.price, M.TimesOrdered
+FROM iMenu M
+WHERE M.TimesOrdered >=50
+ORDER BY TimesOrdered desc;
